3487594f35dd443111fc8478e5fb3e953b7d52c2
samue <samfrk10@gmail.com>
2024-08-24 12:35:40 -0400

    Sam's testing class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Sam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Sam.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@Autonomous(name = "Test_Sam", group = "exercise")
+@Autonomous(name = "Test_Sam", group = "Robot")
 public class Test_Sam extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
