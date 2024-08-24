caa38bdafc58b8974cb0e2a4b400248d7c893ba4
ftcaarya <rautaarya23@gmail.com>
2024-08-24 12:50:19 -0400

    Sam's testing class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAarya.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name = "Testing Aarya", group = "exercise")
+public class TestAarya extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
